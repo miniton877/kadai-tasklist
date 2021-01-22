dace5433757f57f8939f4358b1b945109b904bc1
miniton877 <luckyclover811212@gmail.com>
2021-01-22 21:03:01 +0900

    Error correction and Add index.jsp

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,12 +1,18 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Tasks;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class IndexServlet
@@ -26,7 +32,17 @@ public class IndexServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().append("Served at: ").append(request.getContextPath());
-    }
+      //データベースからテーブル情報を取得する
+        EntityManager em = DBUtil.createEntityManager();
+        List<Tasks> taskList = em.createNamedQuery("getAllTaskList", Tasks.class).getResultList();
+        em.close();
 
+      //index.jspにリクエストスコープで値を渡す
+        request.setAttribute("taskList",  taskList);
+        RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/taskList/index.jsp");
+        rd.forward(request, response);
+    }
 }
+
+
+
